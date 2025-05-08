a36fa5f7d0ca08e9538247112248f5cdd6904850
LucasBoaretto <lcboaretto@hotmail.com>
2025-05-08 16:31:32 -0300

    adicionando componente de dashboard na pagina dashboard

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,12 +1,17 @@
+import Dashboard from "../components/Dashboard";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Main from "../components/Main";
+import NavBar from "../components/NavBar";
 
 function DashboardPage() {
   return (
     <div>
       <Header />
-      <Main />
+      <NavBar />
+      <Main>
+        <Dashboard />
+      </Main>
       <Footer />
     </div>
   );
