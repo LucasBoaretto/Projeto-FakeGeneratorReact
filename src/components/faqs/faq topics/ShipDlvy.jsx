d785f8d590890d111f0abd1c4a42b4e6aff25d24
LucasBoaretto <lcboaretto@hotmail.com>
2025-05-19 18:05:40 -0300

    comentando constantes das faqs para testes de erro com o vercel

--- src/components/faqs/faq topics/ShipDlvy.jsx
+++ src/components/faqs/faq topics/ShipDlvy.jsx
@@ -7,25 +7,25 @@ import FaqSections from "../FaqSections";
 import FaqTopic from "../FaqTopic";
 
 function ShipDlvy() {
-  const shippingAndDelivery = [
-    {
-      question: "HOW MUCH DOES YOUR SHIPPING COST?",
-      answer:
-        "All merch orders from our site of $85+ ship for free on the wings of two-headed demon eagles. Orders under $85 have a standard shipping rate of $6 to help pay the salary of our two-headed demon eagles.",
-    },
+  // const shippingAndDelivery = [
+  //   {
+  //     question: "HOW MUCH DOES YOUR SHIPPING COST?",
+  //     answer:
+  //       "All merch orders from our site of $85+ ship for free on the wings of two-headed demon eagles. Orders under $85 have a standard shipping rate of $6 to help pay the salary of our two-headed demon eagles.",
+  //   },
 
-    {
-      question: "HOW LONG WILL IT TAKE TO GET MY ORDER?",
-      answer: `Merch ordered through liquiddeath.com will ship within 48 hours excluding weekends unless stated
-                    otherwise.<br>
-                    Once shipped, you can expect to receive your order in 2-7 business days depending on the shipping
-                    method purchased. Carrier delays may occur. We apologize if you're going to be naked until then.<br>
-                    Tracking on beverage orders is provided by the retailer you ordered through. Check with them for
-                    updates.`,
-    },
-  ];
+  //   {
+  //     question: "HOW LONG WILL IT TAKE TO GET MY ORDER?",
+  //     answer: `Merch ordered through liquiddeath.com will ship within 48 hours excluding weekends unless stated
+  //                   otherwise.<br>
+  //                   Once shipped, you can expect to receive your order in 2-7 business days depending on the shipping
+  //                   method purchased. Carrier delays may occur. We apologize if you're going to be naked until then.<br>
+  //                   Tracking on beverage orders is provided by the retailer you ordered through. Check with them for
+  //                   updates.`,
+  //   },
+  // ];
 
-  console.log(shippingAndDelivery);
+  // console.log(shippingAndDelivery);
 
   return (
     <FaqSections>
--- src/components/faqs/faq topics/Subscription.jsx
+++ src/components/faqs/faq topics/Subscription.jsx
@@ -7,25 +7,25 @@ import FaqSections from "../FaqSections";
 import FaqTopic from "../FaqTopic";
 
 function Subscription() {
-  const subscription = [
-    {
-      question: "WHY DID MY AUTO DEATH MEMBERSHIP CANCEL?",
-      answer:
-        "On July 5th, 2022 all Auto Death memberships were canceled. We've shifted our online water sales to Amazon where you can Subscribe & Save. Trust us. It'll save you a shitload of money. Up to $4 per case. But you have to resubscribe on Amazon yourself.",
-    },
-    {
-      question: "CAN I RENEW MY AUTO DEATH MEMBERSHIP?",
-      answer:
-        "Here's the bad news: We no longer sell water on liquiddeath.com so you can't renew your membership. Now here's the great news: If you sign up for Subscribe & Save on Amazon you'll get the same great Liquid Death you love but save up to $4 on each case. And you'll get free shipping.",
-    },
-    {
-      question: "HOW DO I CANCEL MY SUBSCRIPTION?",
-      answer:
-        "We already canceled it for you. Go to Amazon.com to Subscribe & Save there. If you need any help click here to reach our surprisingly delightful support team.",
-    },
-  ];
+  // const subscription = [
+  //   {
+  //     question: "WHY DID MY AUTO DEATH MEMBERSHIP CANCEL?",
+  //     answer:
+  //       "On July 5th, 2022 all Auto Death memberships were canceled. We've shifted our online water sales to Amazon where you can Subscribe & Save. Trust us. It'll save you a shitload of money. Up to $4 per case. But you have to resubscribe on Amazon yourself.",
+  //   },
+  //   {
+  //     question: "CAN I RENEW MY AUTO DEATH MEMBERSHIP?",
+  //     answer:
+  //       "Here's the bad news: We no longer sell water on liquiddeath.com so you can't renew your membership. Now here's the great news: If you sign up for Subscribe & Save on Amazon you'll get the same great Liquid Death you love but save up to $4 on each case. And you'll get free shipping.",
+  //   },
+  //   {
+  //     question: "HOW DO I CANCEL MY SUBSCRIPTION?",
+  //     answer:
+  //       "We already canceled it for you. Go to Amazon.com to Subscribe & Save there. If you need any help click here to reach our surprisingly delightful support team.",
+  //   },
+  // ];
 
-  console.log(subscription);
+  // console.log(subscription);
 
   return (
     <FaqSections>
