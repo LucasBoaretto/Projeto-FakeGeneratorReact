57d57b93511eee2d97279653d810d006167ef684
LucasBoaretto <lcboaretto@hotmail.com>
2025-09-02 19:16:41 -0300

    adicionado easteregg

--- src/components/library/itens inLine/Line two buttons/CheckButton.jsx
+++ src/components/library/itens inLine/Line two buttons/CheckButton.jsx
@@ -2,9 +2,11 @@ import { CheckIcon } from "lucide-react";
 
 function CheckButton() {
   return (
-    <button className="bg-green-500 rounded-lg px-2 py-2 cursor-pointer hover:shadow-sm/100">
-      <CheckIcon color="white" size={30} strokeWidth={3}/>
-    </button>
+    <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0" target="blank">
+      <button className="bg-green-500 rounded-lg px-2 py-2 cursor-pointer hover:shadow-sm/100">
+        <CheckIcon color="white" size={30} strokeWidth={3} />
+      </button>
+    </a>
   );
 }
 
